a04ed935273745b66120669e2059122c790f78ad
Tim Baker <Tim.Baker.NZ@gmail.com>
2018-12-14 12:02:45 +1300

    Bug fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,8 +11,7 @@ class Quiz extends Component{
   render() {
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions
-        [this.state.quiz_position - 1]}]
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
       </div>
     )
   }
